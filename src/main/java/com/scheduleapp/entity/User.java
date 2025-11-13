b375dbca1261b129ee7a322c474b3a504a25df26
성종민 <sjm010611@naver.com>
2025-11-13 17:00:15 +0900

    feat: User 엔티티 추가

--- src/main/java/com/scheduleapp/entity/User.java
+++ src/main/java/com/scheduleapp/entity/User.java
@@ -2,6 +2,7 @@ package com.scheduleapp.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,4 +18,28 @@ public class User extends BaseEntity{
     private Long id;
 
     // 유저명
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    // 이메일(unique)
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    // 생성자
+    @Builder
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    // 유저 정보 수정 메서드
+    public void update(String username, String email) {
+        if (username != null && !username.isBlank()) {
+            this.username = username;
+        }
+        if (email != null && !email.isBlank()) {
+            this.email = email;
+        }
+    }
+
 }

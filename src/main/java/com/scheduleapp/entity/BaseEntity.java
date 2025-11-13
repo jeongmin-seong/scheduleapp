9d9821d1b10dae4c5b22ffff0cad9ffe88dcc0a7
성종민 <sjm010611@naver.com>
2025-11-13 15:28:16 +0900

    feat: 공통 날짜 관리를 위한 BaseEntity 추가

---
+++ src/main/java/com/scheduleapp/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.scheduleapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) // Auditing 기능 활성화
+public abstract class BaseEntity {
+
+    // 생성일 - Entity 생성후 저장시 자동으로 현재 시간 저장
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // 수정일 - Entity 수정시 자동으로 현재 시간 저장
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+}

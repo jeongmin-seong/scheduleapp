c879662bea9c33e624ea30a1f975e74ba022db63
성종민 <sjm010611@naver.com>
2025-11-13 15:13:11 +0900

    config: JPA Auditing 설정 추가

---
+++ src/main/java/com/scheduleapp/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.scheduleapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+    // JPA Auditing 기능활성화
+}

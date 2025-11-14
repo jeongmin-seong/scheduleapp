59055b66a13e4270f31ba373bf8aa32c2bfcf979
성종민 <sjm010611@naver.com>
2025-11-14 09:47:07 +0900

    feat: 세션 상수 정의 추가

---
+++ src/main/java/com/scheduleapp/common/SessionConst.java
@@ -0,0 +1,12 @@
+package com.scheduleapp.common;
+
+public class SessionConst {
+
+    // 세션에 저장되는 로그인 유저 ID 키
+    public static final String LOGIN_USER_ID = "LOGIN_USER_ID";
+
+    // 인스턴스화 방지
+    private SessionConst() {
+    }
+}
+

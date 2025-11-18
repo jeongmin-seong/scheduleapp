51d9bea404135ccb0e286cee4bf9f936a2dabcf4
성종민 <sjm010611@naver.com>
2025-11-18 16:43:04 +0900

    fic: Pageable 파라미터 정의

--- src/main/java/com/scheduleapp/repository/ScheduleRepository.java
+++ src/main/java/com/scheduleapp/repository/ScheduleRepository.java
@@ -15,11 +15,14 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     // 모든 일정을 수정일 기준 내림차순으로 조회
     List<Schedule> findAllByOrderByUpdatedAtDesc();
 
+    // 모든 일정을 수정일 기준 내림차순으로 페이징 조회 (페이징 적용)
+    Page<Schedule> findAllByOrderByUpdatedAtDesc(Pageable pageable);
+
     // 특정 사용자의 일정을 수정일 기준 내림차순으로 조회
     List<Schedule> findByUser_IdOrderByUpdatedAtDesc(Long userId);
 
     // 모든 일정을 페이징 처리하여 조회
-    @Query("SELECT s FROM Schedule s JOIN FETCH s.user")
+    @Query("SELECT s FROM Schedule s JOIN FETCH s.user ORDER BY s.updatedAt DESC")
     Page<Schedule> findAllWithUser(Pageable pageable);
 
 }
